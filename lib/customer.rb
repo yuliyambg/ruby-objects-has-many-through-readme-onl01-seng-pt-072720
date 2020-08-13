fa8957471174c13f5efe8dda753419a62b6cf70d
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-13 12:34:24 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,37 @@
 class Customer
+
+attr_accessor :name, :age
+
+@@all = []
+def initialize(name,age)
+  @name = name
+  @age = age
+  @@all << self
+end
+
+
+def self.all
+  @@all
+end
+
+def new_meal(waiter,total,tip)
+  Meal.new(waiter,self,total,tip)
+end
+
+def meals
+  Meal.all.select do |meal|
+    meal.customer ==self
+  end
+end
   
+  def waiters
+  meals.map do |meal|
+    meal.waiter
+  end
+end
+
+
+def best_tipper
+end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
 
+attr_accessor :name, :yrs_experience
+
+@@all = []
+def initialize(name,yrs_experience)
+  @name = name
+  @yrs_experience = yrs_experience
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_meal(customer,total,tip)
+  Meal.new(self,customer,total,tip)
+end
+
+def meals
+  Meal.all.select do |meal|
+    meal.waiter == self
+  end
+end
+
+
 end
